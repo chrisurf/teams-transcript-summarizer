3a6bde625994d21b5896ed6304ffca19ad9eb74f
Chris Oguntolu <44543411+chrisurf@users.noreply.github.com>
2025-03-17 15:23:31 +0100

    Update setup.py with corrected entry point

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "teams-summarize=teams_summarizer:main",
+            "teams-summarize=teams_transcript_summarizer.cli:main",
         ],
     },
 )
